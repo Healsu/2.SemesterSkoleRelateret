5404f61e428280aa9774b6d0397d8e9fbf546c33
Healsu <timmie2500@gmail.com>
2022-02-01 14:46:20 +0100

    Initial commit

---
+++ Skole relateret/Sortering/sort.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class sort {
+    public static void main(String[] args) {
+        ArrayList<String> words = new ArrayList<>();
+        words.add("abekat");
+        words.add("musefælde");
+        words.add("ananas");
+        words.add("slut");
+        words.add("solskin");
+
+
+    }
+}
